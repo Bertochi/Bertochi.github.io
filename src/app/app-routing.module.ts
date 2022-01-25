3b9a994e8f4f4134fee718b4826094238ab1a520
= <=>
2022-01-24 22:33:46 -0300

    production_001

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,45 +4,12 @@ import { HomeComponent } from './views/components/home/home.component';
 import { OrdemComprarComponent } from './views/components/ordem/ordem-comprar/ordem-comprar.component';
 import { OrdemReadComponent } from './views/components/ordem/ordem-read/ordem-read.component';
 import { OrdemVenderComponent } from './views/components/ordem/ordem-vender/ordem-vender.component';
-import { OsClosedComponent } from './views/components/os/os-closed/os-closed.component';
-import { OsCreateComponent } from './views/components/os/os-create/os-create.component';
-import { OsReadComponent } from './views/components/os/os-read/os-read.component';
-import { OsUpdateComponent } from './views/components/os/os-update/os-update.component';
-import { OsViewComponent } from './views/components/os/os-view/os-view.component';
-import { TecnicoCreateComponent } from './views/components/tecnicos/tecnico-create/tecnico-create.component';
-import { TecnicoDeleteComponent } from './views/components/tecnicos/tecnico-delete/tecnico-delete.component';
-import { TecnicoReadComponent } from './views/components/tecnicos/tecnico-read/tecnico-read.component';
-import { TecnicoUpdateComponent } from './views/components/tecnicos/tecnico-update/tecnico-update.component';
+
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'tecnicos/delete/:id',
-    component: TecnicoDeleteComponent
-  },
-
-  {
-    path: 'os',
-    component: OsReadComponent
-  },
-  {
-    path: 'os/closed',
-    component: OsClosedComponent
-  },
-  {
-    path: 'os/create',
-    component: OsCreateComponent
-  },
-  {
-    path: 'os/update/:id',
-    component: OsUpdateComponent
-  },
-  {
-    path: 'os/view/:id',
-    component: OsViewComponent
   },
   {
     path: 'ordem/pesquisa/:id',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-
-import { HeaderComponent } from './views/components/template/header/header.component';
-import { FooterComponent } from './views/components/template/footer/footer.component';
-
 import { HomeComponent } from './views/components/home/home.component';
-import { TecnicoReadComponent } from './views/components/tecnicos/tecnico-read/tecnico-read.component';
-import { TecnicoCreateComponent } from './views/components/tecnicos/tecnico-create/tecnico-create.component';
-import { TecnicoUpdateComponent } from './views/components/tecnicos/tecnico-update/tecnico-update.component';
-import { TecnicoDeleteComponent } from './views/components/tecnicos/tecnico-delete/tecnico-delete.component';
-import { OsReadComponent } from './views/components/os/os-read/os-read.component';
-import { OsCreateComponent } from './views/components/os/os-create/os-create.component';
-import { OsUpdateComponent } from './views/components/os/os-update/os-update.component';
-import { OsViewComponent } from './views/components/os/os-view/os-view.component';
-import { MatMenuModule} from '@angular/material/menu';
-import { OsClosedComponent } from './views/components/os/os-closed/os-closed.component';
-import { OrdemReadComponent } from './views/components/ordem/ordem-read/ordem-read.component';
 import { OrdemComprarComponent } from './views/components/ordem/ordem-comprar/ordem-comprar.component';
+import { OrdemReadComponent } from './views/components/ordem/ordem-read/ordem-read.component';
 import { OrdemVenderComponent } from './views/components/ordem/ordem-vender/ordem-vender.component';
-import { NavComponent } from './views/components/template/nav/nav.component'
+import { FooterComponent } from './views/components/template/footer/footer.component';
+import { HeaderComponent } from './views/components/template/header/header.component';
+import { NavComponent } from './views/components/template/nav/nav.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -44,15 +35,6 @@ import { NavComponent } from './views/components/template/nav/nav.component'
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    TecnicoReadComponent,
-    TecnicoCreateComponent,
-    TecnicoUpdateComponent,
-    TecnicoDeleteComponent,
-    OsReadComponent,
-    OsCreateComponent,
-    OsUpdateComponent,
-    OsViewComponent,
-    OsClosedComponent,
     OrdemReadComponent,
     OrdemComprarComponent,
     OrdemVenderComponent,
